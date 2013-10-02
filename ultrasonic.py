82dc15a247657797ecca08184e4cff4b868833f7
Asa Eckert-Erdheim <Asa@students.olin.edu>
2013-10-02 00:35:38 -0400

    Updates to include plotting functionality.  DOES NOT CURRENTLY WORK.  Exploring matplotlib and mayavi for real time visualization

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,13 +1,22 @@
 import gimbalusb
 import time as t
 import numpy as np
+import pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from random import randint
+from mayavi import mlab 
 
 # Set parameters for servo commands
 MAX_VAL = 2**16 - 1 # 16 bit unsigned int
 MIN_VAL = 0
 
 NUM_STEPS = 25
-INC_VAL = int(MAX_VAL/NUM_STEPS) 
+INC_VAL = int(MAX_VAL/NUM_STEPS)
+
+# Range of motion of servos (deg)
+PAN_RANGE = 180
+TILT_RANGE = 180
 
 # Create list of distances for calibration (cm)
 dists = [10, 20, 30, 40, 50]
@@ -18,37 +27,74 @@ def main():
     PAN = 0
     TILT = 0
 
-    gusb = gimbalusb.gimbalusb()
+    # Initialize gimbal usb object
+    # gusb = gimbalusb.gimbalusb()
 
-    arr = np.zeros([NUM_STEPS, NUM_STEPS, 3])
+    # Start servos at 0 position
+    # gusb.set_vals(PAN, TILT)
 
+    # Run calibration routine
     # speed = calibrate(dists)
 
-    # for pan_ind in range(NUM_STEPS):
-    #     for tilt_ind in range(NUM_STEPS):
-    #         # Send command to board over USB
-    #         gusb.set_vals(PAN, TILT)
+    # Initialize data array
+    arr = np.zeros([NUM_STEPS, NUM_STEPS, 3])
 
-    #         t = gusb.ping_ultrasonic()
-    #         # d = calc_dist(t, speed)
-    #         d = 555
+    # Create plot for data visualization
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
+    # ax.autoscale(True)
 
-    #         arr[pan_ind,tilt_ind] = [PAN, TILT, d]
+    # ax.scatter(arr[:,0], arr[:,1], arr[:,2])
 
-    #         TILT += 1
+    # # plt.show()
 
-    #     PAN += 1
+    # plt.interactive(True)
 
-    while 1:
-        temp = gusb.ping_ultrasonic()
-        t.sleep(0.25)
+    # plt.xlabel('Time (s)')
+    # plt.ylabel()
 
 
-def calc_dist(t,speed):
-    return t * speed
+    # Iterate each servo position
+    for pan_ind in range(NUM_STEPS):
+        for tilt_ind in range(NUM_STEPS):
+            # Send servo position command
+            # gusb.set_vals(PAN, TILT)
+
+
+            # Send ping, measure TOF
+            # tof = gusb.ping_ultrasonic()
+
+            # Use calibrated speed to find dist to object
+            # dist = calc_dist(tof, speed)
+            dist = randint(5,50)
+
+            # Convert spherical coordinates to cartesian
+            [x, y, z] = sphr2cart(PAN, TILT, dist)
+
+            # Store cartestion positions
+            arr[pan_ind,tilt_ind] = [x, y, z]
+            mlab.points3d(arr[:,0], arr[:,1], arr[:,2])
+            mlab.show()
+
+            # Increment tilt value
+            TILT += INC_VAL
+
+        # Increment pan value
+        PAN += INC_VAL
+
+    # ax.scatter(arr[:,0], arr[:,1], arr[:,2])
+
+    # plt.show()
+
+    # plt.show()
+
+    # while 1:
+    #     temp = gusb.ping_ultrasonic()
+    #     t.sleep(0.25)
 
 def calibrate(dists):
 
+    # Calibration routine to calculate distance to object
     arr = np.zeros([len(dists),3])
     i = 0
 
@@ -59,6 +105,32 @@ def calibrate(dists):
         i += 1
 
     return np.average(arr[:,2])
+
+def calc_dist(t,speed):
+
+    # Calculate distance to object using TOF and calibrated speed
+    return t * speed
+
+
+def sphr2cart(pan, tilt, dist):
+
+    # Convert position of servos and object distance to cartesian pos of object
+
+    # Convert servo positions to angles (in rad)
+    theta = np.deg2rad(mapServo2Ang(pan,PAN_RANGE))
+    phi = np.deg2rad(mapServo2Ang(tilt,TILT_RANGE))
+
+    # Convert spherical coordinates to cartesian
+    x = dist * np.cos(theta) * np.sin(phi)
+    y = dist * np.sin(theta) * np.sin(phi)
+    z = dist * np.cos(phi)
+
+    return x, y, z
+
+def mapServo2Ang(servo_val,total_ang):
+
+    # Convert servo command to angle of servo
+    return servo_val / 2**16 * total_ang
 
 if __name__ == '__main__':
     main()
